2056481ca157470960c79a7aad2e9fb9694e5072
Aakash Sharma <sharmaaakash8924@gmai.com>
2023-04-01 15:21:52 +0530

    minor update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -38,7 +38,7 @@ const Projects = () => {
         <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
             <div className="pb-8">
                 <h1 className="text-4xl font-bold inline border-b-4 border-gray-500">Portfolio</h1>
-                <p className="py-6">Check out some of our work right here.</p>
+                <p className="py-6">Check out some of my work right here.</p>
             </div>
            
             <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8">
